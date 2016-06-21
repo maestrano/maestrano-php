cd9c186b3400c6c9d87390f14e2a40c09878647f
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2016-06-21 16:59:14 +1000

    Update phpunit version to stable and fix travisCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 if (!defined('TEST_ROOT')) define('TEST_ROOT', dirname(__FILE__));
 
-require_once TEST_ROOT . '/../lib/Maestrano.php';
+require_once TEST_ROOT . '/../vendor/autoload.php';
 require_once TEST_ROOT . '/support/sso/SessionTestHelper.php';
 require_once TEST_ROOT . '/support/saml/SamlTestHelper.php';
 require_once TEST_ROOT . '/support/stubs/SamlMnoRespStub.php';
